c3e511c9fad02bb015b6f37dc64311bb01af507c
liuxin85 <liuxin85@gmail.com>
2026-06-18 20:24:03 +0800

    feat: ui modification 2

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/data/constants.dart';
+import 'package:flutter_app/views/widgets/container_widget.dart';
 import 'package:flutter_app/views/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,30 +8,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: SingleChildScrollView(
         child: Column(
           children: [
             HeroWidget(title: 'Flutter Mapp'),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Card(
-                child: Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Basic Layout', style: KTextStyle.titleTealText),
-
-                      Text(
-                        'The description of this',
-                        style: KTextStyle.descriptionText,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            ContainerWidget(
+              title: 'Basic layout',
+              description: 'This is a description',
+            ),
+            ContainerWidget(
+              title: 'Basic layout2',
+              description: 'This is a description',
+            ),
+            ContainerWidget(
+              title: 'Basic layout3',
+              description: 'This is a description',
+            ),
+            ContainerWidget(
+              title: 'Basic layout4',
+              description: 'This is a description',
+            ),
+            ContainerWidget(
+              title: 'Basic layout5',
+              description: 'This is a description',
             ),
           ],
         ),
